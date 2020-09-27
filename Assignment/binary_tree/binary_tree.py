8ebf78c211f8ad28cc3b240054da2ba66a96b556
Jiawei Li <iewaijil@gmail.com>
2020-09-27 23:12:08 +0200

    Update BST

--- Assignment/binary_tree/binary_tree.py
+++ Assignment/binary_tree/binary_tree.py
@@ -116,30 +116,30 @@ def add_nodes(a_root, left_list, right_list):
     n_r = len(right_list)
     if n_l == 1 or n_r == 1:
         for i in left_list+right_list:
-            a_root.insert(i.key, i.data)
+            a_root.insert(i[0], i[1])
     else:
         n_l_m = n_l // 2
         n_r_m = n_r // 2
-        a_root.insert(left_list[n_l_m].key, left_list[n_l_m].data)
-        a_root.insert(right_list[n_r_m].key, right_list[n_r_m].data)
+        a_root.insert(left_list[n_l_m][0], left_list[n_l_m][1])
+        a_root.insert(right_list[n_r_m][0], right_list[n_r_m][1])
         add_nodes(a_root, left_list[:n_l_m], left_list[n_l_m+1:])
         add_nodes(a_root, right_list[:n_r_m], right_list[n_r_m+1:])
 
 
 def create_tree(n):
     fake = Faker(["en_US", "en_GB", "fr_FR", "it_IT", "de_DE"])
-    node_list = [Node(i, fake.first_name() + " " + fake.last_name())
-                 for i in range(n)]
+    name_list = [fake.first_name() + " " + fake.last_name() for i in range(n)]
+    sorted_list = list(zip(range(1, n+1), name_list))
     n_m = n // 2
-    tree = Node(node_list[n_m].key, node_list[n_m].data)
-    add_nodes(tree, node_list[:n_m], node_list[n_m+1:])
+    tree = Node(sorted_list[n_m][0], sorted_list[n_m][1])
+    add_nodes(tree, sorted_list[:n_m], sorted_list[n_m+1:])
     return tree
 
 
-def record_complexity(start, end, step):
-    size_array = np.arange(start, end, step)
-    time_array = np.array([])
-    for n in size_array:
+def record_complexity(end, step):
+    size_array = np.arange(0, end+step, step)
+    time_array = np.array([0.])
+    for n in size_array[1:]:
         tree = create_tree(n)
         random_array = np.random.randint(0, n, 1000)
         start = time.time()
@@ -152,5 +152,5 @@ def record_complexity(start, end, step):
     return complexity_data
 
 
-complexity_data = record_complexity(10000, 202000, 2000)
+complexity_data = record_complexity(100000, 2000)
 fig = sns.relplot(x="size", y="time", kind="line", data=complexity_data)
